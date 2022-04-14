6f4dcd3b008f6fd4242706e10a439433c5132fa0
diegoczel <diegoczel@gmail.com>
2022-04-14 17:33:19 -0300

    001_Querying

---
+++ sqlserver/sqlservertutorial/001_Basics/001_Querying/001_Select.sql
@@ -0,0 +1,12 @@
+select
+    columns
+from
+    tables -- joins e tal
+where
+    condition1 = 0
+group by
+    column1 -- using in agregate functions
+having
+    count(*) > 2
+order by
+    columns -- ASC | DESC
